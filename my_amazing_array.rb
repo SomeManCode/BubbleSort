aadf72e841e274f74f9715eb2492107651b7a9d2
Joaquin Gamboa <gamboanj@gmail.com>
2013-04-07 16:21:55 -0400

    Added text for loop process and final result

--- my_amazing_array.rb
+++ my_amazing_array.rb
@@ -1,26 +1,27 @@
-a = [5, 4, 1, 3]
 
-
-    counter = 1
-      while counter > 0 do
-        i = 0
-          #
-        while(i < a.length - 1) do
-          #compares the value of position, and if greater then switches then adds counter
-          if a[i] > a[i+1]
-            a[i], a[i+1] = a[i+1], a[i]
-            counter += 1
-            
-            puts " \n"
-            puts a.join
-          else
+a = [5, 4, 1, 3] 
+      counter = 1
+        while counter > 0 do
+          i = 0
+            #
+            while(i < a.length - 1) do
+            #compares the value of position, and if greater then switches then adds counter
+              if a[i] > a[i+1]
+                a[i], a[i+1] = a[i+1], a[i]
+                counter += 1
+                
+                puts " \n"
+                print "Sorting:   "
+                puts a.join
+              else
+              end
+             i += 1
           end
-        i += 1
+         counter -= 1
         end
-       counter -= 1
-      end
-    print a
+      print "Your array is now sorted to #{a} \n"
 
-    
+
+
 # try each_under_withindex.
 
